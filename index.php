0ec8d911288dfc6c85a9af87ba41426ed0c01717
biurko-pieklo <98983093+biurko-pieklo@users.noreply.github.com>
2023-08-23 06:35:01 +0200

    yeah so no php for me

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once('./theme/header.php');
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus neque debitis iusto itaque ipsum, voluptatum veniam incidunt, odio ex repellat fuga? Dolores officia quam iste expedita esse possimus similique non?</p>
-require_once('./theme/footer.php');
